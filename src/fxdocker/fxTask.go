ac1986900f8ef32792ca8e94c146d3d84836b705
Tigran Bayburtsyan <tigran.bayburtsyan@gmail.com>
2015-06-11 18:49:44 +0400

    correcting tags

--- src/fxdocker/fxTask.go
+++ src/fxdocker/fxTask.go
@@ -304,7 +304,7 @@ func (fxd *FxDaemon) TransferImage(container_cmd map[string]string) (err error) 
 	reg_image = fmt.Sprintf("%s/%s", DockerRegistry, image_names[0])
 
 	err = client.PullImage(docker.PullImageOptions{
-		Repository: image_names[0],
+		Repository: reg_image,
 		Registry: DockerRegistry,
 		Tag: image_names[1],
 		OutputStream:os.Stdout,
