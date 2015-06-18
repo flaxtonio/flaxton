b82061d24c9e66e0bc6fa5aa59a3fa43fea5aeb3
Tigran Bayburtsyan <tigran.bayburtsyan@gmail.com>
2015-06-18 11:51:05 +0400

    making specifications for Server ... in Abu Dabi to LA plain :)

--- src/fxSocket/fxSocket.go
+++ src/fxSocket/fxSocket.go
@@ -1,16 +1,46 @@
 package fxSocket
 
+import (
+	"golang.org/x/net/websocket"
+	"net/http"
+	"fmt"
+)
+
 type SocketEvent struct {
-	Name        	string 				`json:"name"`
-	Handler     	func(interface{}) 	`json:"-"`
-	HandlerData 	interface{}         `json:"handler_data"`
+	Name        	string 					`json:"name"`
+	Handler     	func(*websocket.Conn) 	`json:"-"`
+	HandlerData 	interface{}         	`json:"handler_data"`
 }
 
 type Server struct {
-	ListenAddress string `json:"listen_address"`
-	Events []SocketEvent `json:"events"`
+	ListenAddress 	string 					`json:"listen_address"`
+	/**
+	Events with key -> value map , where key - event name , value - event handler function
+	 */
+	Events 			map[string]SocketEvent 	`json:"events"`
+	ServerMux 		*http.ServeMux 			`json:"-"`
 }
 
 type Client struct {
-	Events []SocketEvent `json:"events"`
+	ServerAddress 	string 			`json:"server_address"`
+	Events 			[]SocketEvent 	`json:"events"`
+}
+
+func NewServer(listen_addr string) (srv Server) {
+	srv.ServerMux = http.NewServeMux()
+	srv.ListenAddress = listen_addr
+	return
+}
+
+func (s *Server) Listen() (err error) {
+	// Setting socket event handlers here
+	for _, ev := range s.Events {
+		s.ServerMux.Handle(fmt.Sprintf("/%s", ev.Name), websocket.Handler(ev))
+	}
+	err = http.ListenAndServe(s.ListenAddress, s.ServerMux)
+	return
+}
+
+func (s *Server) AddEvent() {
+
 }
