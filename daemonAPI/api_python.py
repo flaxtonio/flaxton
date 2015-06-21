92860548e2c6a0bcb3e4b2e9f63d2215ba885233
Tigran Bayburtsyan <tigran.bayburtsyan@gmail.com>
2015-06-21 21:04:09 +0400

    adding free to use url

--- daemonAPI/api_python.py
+++ daemonAPI/api_python.py
@@ -1,25 +1,27 @@
 import json
 import requests
-import urllib2
 import time
 
 def AddContainerCall(container_id, daemon):
 	url = 'http://container.flaxton.io/api/task/add'
-	send_data = {'task_type' : 'start_container',
+	send_data = {'task_type' : 'pause_container',
 		'daemon' : daemon,
 		'data': [container_id]
 	}
 	r = requests.post(url, data=send_data)
+	print(r.text)
 
 last_container = {}
 
 while True:
-	data = json.load(urllib2.urlopen('http://container.flaxton.io/daemon-state'))
+	r = requests.get('http://container.flaxton.io/daemon-state')
+	data = json.loads(r.text)
 	for daemon, info in data.items():
-		print len(info["data"]["containers"].keys())
-		if len(info["data"]["containers"].keys()) == 2:
-			print last_container
+		print(len(info["data"]["containers"].keys()))
+		if len(info["data"]["containers"].keys()) == 2 and len(last_container) != 0:
+			print(last_container)
 			AddContainerCall(last_container, daemon)
+			exit(1)
 		else:
-			last_container = info["data"]["containers"].keys()[0]
+			last_container = list(info["data"]["containers"].keys())[0]
 	time.sleep(2)
