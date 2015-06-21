0a3b97314c8a8185a94942d0ed332ef002852784
Tigran Bayburtsyan <tigran.bayburtsyan@gmail.com>
2015-06-21 09:34:22 +0400

    rolling back to Node.js -> don't have time to rewrite all daemon logic to Go

--- src/fxBackend/fxBackend.go
+++ src/fxBackend/fxBackend.go
@@ -4,3 +4,13 @@ import (
 	"fxSocket"
 )
 
+var BackendService fxSocket.Parent
+
+func Start(listen_address string) (err error) {
+	BackendService, err = fxSocket.NewParent(listen_address)
+	return
+}
+
+func RegisterEvents() {
+
+}
